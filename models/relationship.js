b8f1d2c4ea1ee18eceacfaa4207b66c16ad49027
dusainbolt <dulh181199@gmail.com>
2020-11-18 19:25:32 +0700

    Merge branch 'dev' of https://github.com/thanhthinh95/lgbt_viet_nam_server into dev_send_email

--- models/relationship.js
+++ models/relationship.js
@@ -1,8 +1,9 @@
 const constants = require(`../constants/constants.js`);
 
-let obj_schema = new _mongoose.Schema({
-    userFirstId: { type: _mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-    userSecondId: { type: _mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+let obj_schema = new _mongoose.Schema(
+  {
+    userFirstId: { type: _mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    userSecondId: { type: _mongoose.Schema.Types.ObjectId, ref: "user", required: true },
 
     showStoryFirst: { type: Boolean, default: true },
     showStorySecond: { type: Boolean, default: true },
@@ -11,10 +12,12 @@ let obj_schema = new _mongoose.Schema({
     updated: { type: Date, default: null },
 
     status: { type: Number, default: constants.RELATIONSHIP.STATUS.ACTIVE }, //1: Hoạt động, 2: Đã bị chặn
-}, { id: false, versionKey: 'v' });
+  },
+  { id: false, versionKey: "v" }
+);
 
 obj_schema.index({ userFirstId: 1, userSecondId: 1 });
-obj_schema.set('toJSON', { getters: true });
-obj_schema.set('toObject', { getters: true });
+obj_schema.set("toJSON", { getters: true });
+obj_schema.set("toObject", { getters: true });
 
-module.exports = _mongoose.model('relationship', obj_schema);
+module.exports = _mongoose.model("relationship", obj_schema);
