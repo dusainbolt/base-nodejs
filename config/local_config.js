6b8998f0bd7cb6f99c2b893913c1cc3696143adb
dusainbolt <dulh181199@gmail.com>
2020-11-19 09:51:03 +0700

    localconfig

---
+++ config/local_config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    SERVER_PORT: `6630`,
+    APP_KEY: `ag123FSG$#6546`,
+    MONGODB: {
+        HOST: `localhost`,
+        PORT: `27017`,
+        USERNAME: `lgbt_user`,
+        PASSWORD: `12345678`,
+        NAME: `lgbt_vietnam`,
+        SRV: ``,
+    },
+    REDIS: {
+        HOST: `localhost`,
+        PORT: `6379`,
+        PASSWORD: `tr@$234gGFjgfh35`,
+    },
+    ELASTIC: {
+        HOST: `localhost`,
+        PORT: `9200`,
+        USERNAME: `elastic`,
+        PASSWORD: `JHGFLKJO234@$dsa`,
+    },
+    EMAIL: {
+        HOST: 'smtp.gmail.com',
+        PORT: 587,
+        USERNAME: 'appdu.hotro@gmail.com',
+        PASSWORD: 'ylnkfupxfeynqprv',
+        FROM_NAME: 'App Du Hỗ Trợ',
+    },
+    JWT: {
+        PRIVATE_KEY: `key_123543%G)G`,
+        AGE: `5d`,
+    },
+    BCRYPT: {
+        SALT: 10
+    },
+    SIZE_FILE_LIMIT: `15mb`,
+};
